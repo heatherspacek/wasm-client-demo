e6e2f7d28aa09cd7ca8f918cdb03d1b2673e73b2
Heather Spacek <heatherspacek1997@gmail.com>
2026-08-30 22:40:42 -0600

    draw timer in seconds

--- src/timers.c
+++ src/timers.c
@@ -4,18 +4,22 @@
 void set_and_start_timer(int init_seconds, void *callback, Timer *timer) {
     timer->ms_remaining = init_seconds * 1000;
     timer->active = 1;
+    timer->visible = 1;
     timer->callback = callback;
 }
 
 void fire_and_stop_timer(Timer *timer) {
     timer->ms_remaining = 0;
     timer->active = 0;
+    timer->visible = 0;
     timer->callback();
 }
 
 void draw_timer(int x, int y, Timer *timer) {
-
-    _render_int(x, y, timer->ms_remaining);
+    if (!timer->visible) return;
+    // todo: circle timer thingy!
+    int sec_remaining = (int)(timer->ms_remaining * 0.001);
+    _render_int(x, y, sec_remaining);
 }
 
 void _tick_timer(int ms_elapsed, Timer *timer) {
